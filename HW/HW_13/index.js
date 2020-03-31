97acf7b2a8bbd539ebe92555e615d9389d767d75
AntonChetveryk <chetverykanton92@gmail.com>
2020-03-31 23:33:05 +0300

    fs

--- HW/HW_13/index.js
+++ HW/HW_13/index.js
@@ -9,3 +9,22 @@ const idGenerator = createIdGenerator();
 
 console.log(idGenerator.next().value);
 console.log(idGenerator.next().value);
+console.log("                  Task2");
+
+function* FontGenerator(fontSize) {
+  while (true) {
+    let value = yield fontSize;
+    if (value === "up") {
+      fontSize++;
+    }
+    if (value === "down") {
+      fontSize--;
+    }
+  }
+}
+
+const fontGenerator = FontGenerator(14);
+console.log(fontGenerator.next().value);
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next().done);
